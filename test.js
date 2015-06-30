5d55e0dce4a832528c3d25936cd195c933387f1c
Qix <i.am.qix@gmail.com>
2015-06-29 19:42:44 -0600

    added lego transformer

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 var should = require('should');
 
 var LegoFilter = require('./lib/lego-filter');
+var LegoTransform = require('./lib/lego-transform');
 
 describe('filter', function() {
   it('should allow matching pieces of data', function(done) {
@@ -22,5 +23,32 @@ describe('filter', function() {
 
     filter.input({foo:1234});
     setTimeout(done, 20);
+  });
+});
+
+describe('transform', function() {
+  it('should transform simple values', function(done) {
+    var filter = new LegoTransform({foo:3});
+    filter.on('data', function(v) {
+      v.should.have.property('foo', 3);
+      done();
+    });
+    filter.input({foo:'bar'});
+  });
+  it('should transform regex values', function(done) {
+    var filter = new LegoTransform({foo:[/^b/, 'm']});
+    filter.on('data', function(v) {
+      v.should.have.property('foo', 'mar');
+      done();
+    });
+    filter.input({foo:'bar'});
+  });
+  it('should ignore invalid transforms', function(done) {
+    var filter = new LegoTransform({qux:[/^b/, 'm']});
+    filter.on('data', function(v) {
+      v.should.have.property('foo', 'bar');
+      done();
+    });
+    filter.input({foo:'bar'});
   });
 });
